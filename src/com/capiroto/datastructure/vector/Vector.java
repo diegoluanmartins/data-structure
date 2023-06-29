b8517482dbba7e941f7447df02e6ad53778a5724
diegoluan <diegoluan@weg.net>
2023-06-29 20:58:47 -0300

    Aula01 até Aula06

--- src/com/capiroto/datastructure/vector/Vector.java
+++ src/com/capiroto/datastructure/vector/Vector.java
@@ -1,38 +1,36 @@
 package com.capiroto.datastructure.vector;
 
-import java.util.Arrays;
-
 public class Vector {
-    
+
     private String[] elements;
     private int size;
 
-    public Vector(int size){
+    public Vector(int size) {
         this.elements = new String[size];
         this.size = 0;
     }
 
     // public void add(String element){
-    //     for(int i=0; i < this.elements.length; i++){
-    //         if(this.elements[i] == null){
-    //             this.elements[i] = element;
-    //             break;
-    //         }
-    //     }
+    // for(int i=0; i < this.elements.length; i++){
+    // if(this.elements[i] == null){
+    // this.elements[i] = element;
+    // break;
+    // }
+    // }
     // }
 
     // public void add(String element) throws Exception{
-    //     if (size < this.elements.length){
-    //         this.elements[size] = element;
-    //         this.size++;
-    //     }
-    //     else{
-    //         throw new Exception("out of bounds");
-    //     }
+    // if (size < this.elements.length){
+    // this.elements[size] = element;
+    // this.size++;
+    // }
+    // else{
+    // throw new Exception("out of bounds");
+    // }
     // }
 
-    public boolean add(String element){
-        if (size < this.elements.length){
+    public boolean add(String element) {
+        if (size < this.elements.length) {
             this.elements[size] = element;
             this.size++;
             return true;
@@ -40,20 +38,35 @@ public class Vector {
         return false;
     }
 
-    public int size(){
+    public int size() {
         return this.size;
     }
 
+    public String get(int pos) {
+        if (pos > size - 1 || pos < 0)
+            throw new IllegalArgumentException("Invalid position");
+        return elements[pos];
+    }
+
+    public int get(String obj) {
+        for (int i = 0; i < this.elements.length; i++) {
+            if (obj.equals(this.elements[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder s = new StringBuilder();
         s.append("[");
-        for (int i = 0; i < size -1; i++){
+        for (int i = 0; i < size - 1; i++) {
             s.append(this.elements[i]);
             s.append(", ");
         }
 
-        if (this.size > 0){
+        if (this.size > 0) {
             s.append(this.elements[this.size - 1]);
         }
 
